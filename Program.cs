e8e677e41966e1b5ad27654cb498e7f47627b8df
Zahra-GLMD <z.golmohammadi00@gmail.com>
2024-07-18 16:41:52 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RegexSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string phoneNumber = "09123456789";
+            Console.WriteLine(phoneNumber.ValidatePhoneNumber());
+            string emailAddress = "shariati402@ut.ac.ir";
+            Console.WriteLine(emailAddress.ValidateEmailAddress());
+            string urlAddress = "https://bootcamp4.maktabsharif.ir/html5client/join?sessionToken=vmwasqqilz10nomx";
+            Console.WriteLine(urlAddress.ValidateUrlAddress());
+
+        }
+    }
+}
---
+++ RegexHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexSample
+{
+    public static class RegexHelper
+    {
+        private const string phoneNumberPattern = @"^(\+98|0)9\d{9}$";
+        private const string emailPattern = @"^[\w\d_.]+@[\w]+(\.[\w]{2,})+$";
+        private const string urlPattern = @"^(https?:\/\/)?[\w\d.-]+\.[\w]{2,}(:\d+)?(\/[\w\d._#~%+=,-?&]*)*$";
+
+        public static bool ValidatePhoneNumber(this string phoneNumber)
+        {
+            Regex regex = new Regex(phoneNumberPattern);
+            return regex.IsMatch(phoneNumber);
+        }
+        public static bool ValidateEmailAddress(this string emailAddress)
+        {
+            Regex regex = new Regex(emailPattern);
+            return regex.IsMatch(emailAddress);
+        }
+        public static bool ValidateUrlAddress(this string urlAddress)
+        {
+            Regex regex = new Regex(urlPattern);
+            return regex.IsMatch(urlAddress);
+        }
+    }
+    
+}
